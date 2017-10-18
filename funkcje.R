8c877649953b9a6994a70a86eb2d6e9f44bb8409
Vecti <32849127+Vecti@users.noreply.github.com>
2017-10-18 13:36:48 +0200

    Add files via upload

---
+++ funkcje.R
@@ -0,0 +1,26 @@
+#' Square number
+#'
+#'Takes any numeric and square it
+#'
+#' @param x   A numeric value to the squared
+#' @return The square
+#' @usage square(x)
+#' @example    5^2=25
+#' @example    6^2=36
+#'
+#' @export
+
+square<-function(x ) x^2
+
+
+#' to the power of
+#'
+#' @description  This function calculated x to the power of n
+#' @param x  Base
+#' @param n  exponent
+#' @usage cube(x,n)
+#' @return x to the power of n
+#'
+#' @export
+
+potega<-function(x,n) x^n
---
+++ lifetable.R
@@ -0,0 +1,14 @@
+#' Opis danych
+#'
+#' Dane te zawieraja informacje pobrane z GUS-u, są to tak zwane tablice życia("life table")
+#' Poszczególne kolumny zawieraja
+#' \itemize{
+#' \item wiek. Opisuje ona dany wiek
+#' \item liczba dożywających. Opisuje ona ilość osób dożywająca do danego wieku(na 100000 ludzi)
+#' \item liczba zmarlych. Opisuje ona procentowa ilosc ludzi co umiera w tym wieku
+#' \item przeciętne trwanie życia. Opisuje ona to co sugeruje tytuł.
+#' }
+#' @format A data frame with 101 obs and 4 variables.
+#' @docType data
+#' @name lifetable
+#' @export
